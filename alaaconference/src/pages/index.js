61bdf8ec48d8fb86ac0729bc6c0fa0f30ebe4a47
Mark Ferraretto <mark@ferraretto.com>
2024-03-21 12:23:07 +1030

    Updated title displayed in browser tab

--- alaaconference/src/pages/index.js
+++ alaaconference/src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`2024 ALAA Conference`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
