90a04794c7ff26f0a4bc8aff4a9eade6989ac2aa
Mark Ferraretto <mark@ferraretto.com>
2024-04-04 14:28:43 +1030

    Restore Docassemble page

--- alaaconference/src/components/EOIButton/index.js
+++ alaaconference/src/components/EOIButton/index.js
@@ -5,7 +5,8 @@ const EOIButton = () => {
   return (
     <div className="buttonContainer">
       <a
-        href="https://forms.office.com/Pages/ResponsePage.aspx?id=u30Vcr3o3ECNAD8IwjSsR5d-9ebgQuFCqx0MmfvIP1hUMjYzMFRUOFkyWU1OWDhTWDVXMVg0S0FTMC4u"
+        /* href="https://forms.office.com/Pages/ResponsePage.aspx?id=u30Vcr3o3ECNAD8IwjSsR5d-9ebgQuFCqx0MmfvIP1hUMjYzMFRUOFkyWU1OWDhTWDVXMVg0S0FTMC4u" */
+        href='https://docassemble2.flinders.edu.au/interview?i=docassemble.2024ALAAConference:data/questions/eoi.yml'
         className="interestButton"
         target='_blank'
         rel='noopener noreferrer'
