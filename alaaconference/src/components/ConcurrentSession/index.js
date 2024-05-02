9e6ab6b9d28441a1deae34f6c0ef04b6c43754f4
Mark Ferraretto <mark@ferraretto.com>
2024-05-02 22:27:25 +0930

    Working on displaying the sessions

--- alaaconference/src/components/ConcurrentSession/index.js
+++ alaaconference/src/components/ConcurrentSession/index.js
@@ -19,3 +19,52 @@ export function ConcurrentSession({title, presenters}) {
 
 }
 
+// Prints the table row for a list of sessions.  
+// Assume we have three sessions per row.
+// The first row contains:
+// - session identifier
+// - session title
+// - link to abstract
+// - session presenter(s)
+// The second row contains a link to the padlet for this session
+
+export function CSRow({sessions}) {
+    // sessions should contain:
+    // - title
+    // - presenters
+    // - abstractURL
+    // - learnURL
+    return (
+        <>
+            <tr>
+                {sessions.map((session, index) => (
+                    <td>
+                        <ConcurrentSession
+                            title={session.title}
+                            presenters={session.presenters} />
+                    </td>
+                ))}
+            </tr>
+            <tr>
+                    {sessions.map((session, index) => (
+                        <td>
+                            <LearnButton learnURL={session.learnURL} />
+                        </td>
+                    ))}
+            </tr>
+        </>
+    )
+}
+
+const morning_session = ['1', '2', '3', '4'];
+const streams = ['A', 'B', 'C'];
+
+export function DisplayConcurrentSession() {
+    const display_sessions = []
+    streams.forEach(stream => {
+
+
+    })
+
+    
+}
---
+++ alaaconference/src/components/ConcurrentSession/sessionData.js
@@ -0,0 +1,55 @@
+// Dictionary. Contains the information about each session.
+// Main key is 'id' plus the session ID (eg: '!A', '3B' etc)
+// Structure is this:
+// id1A : {
+//     title
+//     presenters
+//     abstractURL
+//     learnURL
+// }
+
+const sessionData = {
+    id1A : {
+        title: 'Integrating Emerging Technology and Property Law Doctrine in Undergraduate and Postgraduate Property and Trusts Law Teaching',
+        presenters: 'Brendan Grigg, Flinders University',
+        abstractURL: 'tbc',
+        learnURL: 'TBC1'
+    },
+    id2A : {
+        title: '2Integrating Emerging Technology and Property Law Doctrine in Undergraduate and Postgraduate Property and Trusts Law Teaching',
+        presenters: 'Brendan Grigg, Flinders University',
+        abstractURL: 'tbc',
+        learnURL: 'TBC2'
+    },
+    id3A : {
+        title: '3Integrating Emerging Technology and Property Law Doctrine in Undergraduate and Postgraduate Property and Trusts Law Teaching',
+        presenters: 'Brendan Grigg, Flinders University',
+        abstractURL: 'tbc',
+        learnURL: 'TBC3'
+    },
+    id4A : {
+        title: '3Integrating Emerging Technology and Property Law Doctrine in Undergraduate and Postgraduate Property and Trusts Law Teaching',
+        presenters: 'Brendan Grigg, Flinders University',
+        learnURL: 'TBC3'
+    },
+    id1B : {
+        title: 'Integrating Emerging Technology and Property Law Doctrine in Undergraduate and Postgraduate Property and Trusts Law Teaching',
+        presenters: 'Brendan Grigg, Flinders University',
+        learnURL: 'TBC1'
+    },
+    id2B : {
+        title: '2Integrating Emerging Technology and Property Law Doctrine in Undergraduate and Postgraduate Property and Trusts Law Teaching',
+        presenters: 'Brendan Grigg, Flinders University',
+        learnURL: 'TBC2'
+    },
+    id3B : {
+        title: '3Integrating Emerging Technology and Property Law Doctrine in Undergraduate and Postgraduate Property and Trusts Law Teaching',
+        presenters: 'Brendan Grigg, Flinders University',
+        learnURL: 'TBC3'
+    },
+    id4B : {
+        title: '3Integrating Emerging Technology and Property Law Doctrine in Undergraduate and Postgraduate Property and Trusts Law Teaching',
+        presenters: 'Brendan Grigg, Flinders University',
+        learnURL: 'TBC3'
+    },
+}
